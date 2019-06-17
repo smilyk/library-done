f12d443cb88f9314321f3306a3454c65c8fe8185
Natalya <smilyk1982@gmail.com>
2019-06-17 11:18:21 +0300

    added most popular books

--- src/telran/library/controller/LibraryRestControllerAppl.java
+++ src/telran/library/controller/LibraryRestControllerAppl.java
@@ -12,67 +12,77 @@ import telran.library.dto.*;
 import telran.library.service.ILibrary;
 @RestController
 public class LibraryRestControllerAppl {
-@Autowired
-ILibrary library;
-@PreDestroy
-public void displayRedearsDelayers() {
-	System.out.println("List of delayers:");
-	library.getReadersDelayingBooks().forEach(System.out::println);
-}
-@PostMapping(value=LibraryApiConstants.ADD_BOOK)
-LibraryReturnCode addBook(@RequestBody BookDto book) {
-	return library.addBook(book);
-}
-@PostMapping(value=LibraryApiConstants.ADD_AUTHOR)
-LibraryReturnCode addAuthor(@RequestBody AuthorDto author) {
-	return library.addAuthor(author);
-}
-@PostMapping(value=LibraryApiConstants.PICK_BOOK)
-LibraryReturnCode pickBook(@RequestBody PickBookData pbd) {
-	return library.pickBook(pbd.readerId, pbd.isbn,
-			LocalDate.parse(pbd.pickDate));
-}
-@PostMapping(value=LibraryApiConstants.RETURN_BOOK)
-LibraryReturnCode returnBook (@RequestBody ReturnBookData rbd)
-{
-	try {
-		LocalDate returnDate=LocalDate.parse(rbd.returnDate);
-		return library.returnBook(rbd.readerId, rbd.isbn, returnDate);
-	} catch (Exception e) {
-		return LibraryReturnCode.WRONG_DATE_FORMAT;
+	@Autowired
+	ILibrary library;
+
+	@PreDestroy
+	public void displayRedearsDelayers() {
+		System.out.println("List of delayers:");
+		library.getReadersDelayingBooks().forEach(System.out::println);
 	}
-}
 
-@PostMapping(value=LibraryApiConstants.ADD_READER)
-LibraryReturnCode addReader(@RequestBody ReaderDto reader) {
-	return library.addReader(reader);
-}
-@DeleteMapping(value=LibraryApiConstants.REMOVE_AUTHOR)
-List<BookDto> removeAuthor(@PathVariable String authorName){
-	return library.removeAuthor(authorName);
-}
-@GetMapping(value=LibraryApiConstants.GET_READERS_DELAYING)
-List<ReaderDto> readersDelaying(){
-	return library.getReadersDelayingBooks();
-}
-@GetMapping(value=LibraryApiConstants.GET_BOOK_AUTHORS)
-List<AuthorDto> getBookAuthors(@PathVariable(name=
-LibraryApiConstants.ISBN) long isbn){
-	return library.getBookAuthors(isbn);
-}
+	@PostMapping(value = LibraryApiConstants.ADD_BOOK)
+	LibraryReturnCode addBook(@RequestBody BookDto book) {
+		return library.addBook(book);
+	}
 
-@GetMapping(value = LibraryApiConstants.ACTIVE)
-	List<ReaderDto> getActiveReaders(){
-	return library.getMostActiveReaders();
-}
+	@PostMapping(value = LibraryApiConstants.ADD_AUTHOR)
+	LibraryReturnCode addAuthor(@RequestBody AuthorDto author) {
+		return library.addAuthor(author);
+	}
+
+	@PostMapping(value = LibraryApiConstants.PICK_BOOK)
+	LibraryReturnCode pickBook(@RequestBody PickBookData pbd) {
+		return library.pickBook(pbd.readerId, pbd.isbn,
+				LocalDate.parse(pbd.pickDate));
+	}
+
+	@PostMapping(value = LibraryApiConstants.RETURN_BOOK)
+	LibraryReturnCode returnBook(@RequestBody ReturnBookData rbd) {
+		try {
+			LocalDate returnDate = LocalDate.parse(rbd.returnDate);
+			return library.returnBook(rbd.readerId, rbd.isbn, returnDate);
+		} catch (Exception e) {
+			return LibraryReturnCode.WRONG_DATE_FORMAT;
+		}
+	}
+
+	@PostMapping(value = LibraryApiConstants.ADD_READER)
+	LibraryReturnCode addReader(@RequestBody ReaderDto reader) {
+		return library.addReader(reader);
+	}
+
+	@DeleteMapping(value = LibraryApiConstants.REMOVE_AUTHOR)
+	List<BookDto> removeAuthor(@PathVariable String authorName) {
+		return library.removeAuthor(authorName);
+	}
+
+	@GetMapping(value = LibraryApiConstants.GET_READERS_DELAYING)
+	List<ReaderDto> readersDelaying() {
+		return library.getReadersDelayingBooks();
+	}
+
+	@GetMapping(value = LibraryApiConstants.GET_BOOK_AUTHORS)
+	List<AuthorDto> getBookAuthors(@PathVariable(name =
+			LibraryApiConstants.ISBN) long isbn) {
+		return library.getBookAuthors(isbn);
+	}
+
+	@GetMapping(value = LibraryApiConstants.ACTIVE)
+	List<ReaderDto> getActiveReaders() {
+		return library.getMostActiveReaders();
+	}
 
 
-@GetMapping(value=LibraryApiConstants.GET_AUTHOR_BOOKS)
-List<BookDto> getAuthorBooks(@PathVariable(name=
-LibraryApiConstants.AUTHOR_NAME) String authorName){
-	return library.getAuthorBooks(authorName);
-}
+	@GetMapping(value = LibraryApiConstants.GET_AUTHOR_BOOKS)
+	List<BookDto> getAuthorBooks(@PathVariable(name =
+			LibraryApiConstants.AUTHOR_NAME) String authorName) {
+		return library.getAuthorBooks(authorName);
+	}
 
-	
+	@GetMapping("/books/mostpopular/{yearFrom}/{yearTo}")
+	public List<BookDto> getMostPopularBooks(@PathVariable int yearFrom, @PathVariable int yearTo) {
+		return library.getMostPopularBooks(yearFrom, yearTo);
+	}
 
 }
